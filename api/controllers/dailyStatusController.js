90b16914206f9d40b3810c16b135bd4ea8a8d612
Borg Fu <cxd873@motorolasolutions.com>
2018-11-30 17:57:24 +0800

    Add server

---
+++ api/controllers/dailyStatusController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const util = require('util');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+
+class dailyStatusController {
+  static getDailyStatusByDate (req, res) {
+    
+  }
+}
+
+module.exports = {
+  hello: hello
+};
